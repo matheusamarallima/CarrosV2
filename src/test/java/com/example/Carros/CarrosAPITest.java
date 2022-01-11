e77793e7ada922f7cccc0050c646c5d369124e30
Matheus Amaral Lima <MatheusAmaral.Lima@mastercard.com>
2022-01-11 16:47:49 -0600

    Class 71, new service established and working, role capability now functional
    Heroku Not working

--- src/test/java/com/example/Carros/CarrosAPITest.java
+++ src/test/java/com/example/Carros/CarrosAPITest.java
@@ -23,35 +23,39 @@ public class CarrosAPITest {
     @Autowired
     protected TestRestTemplate rest;
 
+    @Autowired
+    private CarrosService service;
 
     private ResponseEntity<CarroDTO> getCarro(String url) {
-        return rest.getForEntity(url, CarroDTO.class);
+        return
+                rest.withBasicAuth("user","123").getForEntity(url, CarroDTO.class);
     }
 
     private ResponseEntity<List<CarroDTO>> getCarros(String url) {
-        return rest.exchange(url,
+        return rest.withBasicAuth("user","123").exchange(
+                url,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<CarroDTO>>() {
-        });
-
-
+                });
     }
 
+
     @Test
-    public void testSave(){
+    public void testSave() {
+
         Carro carro = new Carro();
         carro.setNome("Porshe");
         carro.setTipo("esportivos");
 
-        //Teste de inserção
-        ResponseEntity response = rest.postForEntity("api/v1/carros", carro, null);
+        // Insert
+        ResponseEntity response = rest.withBasicAuth("admin","123").postForEntity("/api/v1/carros", carro, null);
         System.out.println(response);
 
-        //Verfica a criação
+        // Verifica se criou
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
-        //Busca o objeto
+        // Buscar o objeto
         String location = response.getHeaders().get("location").get(0);
         CarroDTO c = getCarro(location).getBody();
 
@@ -59,31 +63,33 @@ public class CarrosAPITest {
         assertEquals("Porshe", c.getNome());
         assertEquals("esportivos", c.getTipo());
 
-        //Deleta
-        rest.delete(location);
+        // Deletar o objeto
+        rest.withBasicAuth("user","123").delete(location);
 
-        //verifica se apagou
+        // Verificar se deletou
         assertEquals(HttpStatus.NOT_FOUND, getCarro(location).getStatusCode());
-
     }
 
     @Test
-    public void testLista(){
+    public void testLista() {
         List<CarroDTO> carros = getCarros("/api/v1/carros").getBody();
         assertNotNull(carros);
         assertEquals(30, carros.size());
     }
+
     @Test
-    public void testListaPorTipo(){
-        assertEquals(10, getCarros("/api/v1/carros/classicos").getBody().size());
-        assertEquals(10, getCarros("/api/v1/carros/esportivos").getBody().size());
-        assertEquals(10, getCarros("/api/v1/carros/luxo").getBody().size());
+    public void testListaPorTipo() {
+
+        assertEquals(10, getCarros("/api/v1/carros/tipo/classicos").getBody().size());
+        assertEquals(10, getCarros("/api/v1/carros/tipo/esportivos").getBody().size());
+        assertEquals(10, getCarros("/api/v1/carros/tipo/luxo").getBody().size());
 
         assertEquals(HttpStatus.NO_CONTENT, getCarros("/api/v1/carros/tipo/xxx").getStatusCode());
     }
 
     @Test
-    public void testGetOk(){
+    public void testGetOk() {
+
         ResponseEntity<CarroDTO> response = getCarro("/api/v1/carros/11");
         assertEquals(response.getStatusCode(), HttpStatus.OK);
 
@@ -92,7 +98,8 @@ public class CarrosAPITest {
     }
 
     @Test
-    public void testGetNotFound(){
+    public void testGetNotFound() {
+
         ResponseEntity response = getCarro("/api/v1/carros/1100");
         assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
     }
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,3 +1,34 @@
+
+create table user (
+     id bigint not null auto_increment,
+      email varchar(255),
+      login varchar(255),
+      nome varchar(255),
+      senha varchar(255),
+      primary key (id)
+  );
+
+create table role (
+   id bigint not null auto_increment,
+    nome varchar(255),
+    primary key (id)
+);
+
+create table user_roles (
+   user_id bigint not null,
+   role_id bigint not null
+);
+
+alter table user_roles
+   add constraint FK_user_roles_role
+   foreign key (role_id)
+   references role (id);
+
+alter table user_roles
+   add constraint FK_user_roles_user
+   foreign key (user_id)
+   references user (id);
+
 create table carro
     (id bigint not null auto_increment,
     nome varchar(255),
@@ -7,3 +38,4 @@ create table carro
     latitude varchar(255),
     longitude varchar(255),
     tipo varchar(255), primary key (id) );
+
